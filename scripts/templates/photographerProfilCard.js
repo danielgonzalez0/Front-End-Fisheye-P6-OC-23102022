1aa6200891bda85129e74977afb8c5ff4c07085c
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2022-12-02 11:15:57 +0100

    media queries page photographer

--- scripts/templates/photographerProfilCard.js
+++ scripts/templates/photographerProfilCard.js
@@ -7,7 +7,7 @@ export class PhotographerProfilCard {
     const div = document.createElement('div');
     div.classList.add('photograph-profil');
     const photographerProfilCard = `
-    <div class="text-container"> 
+    <div class="text-container" id="profil-content"> 
           <h1>${this._photographer.name}</h1>
           <p class="city">${this._photographer.city}, ${this._photographer.country}</p>
           <p class="tagline">${this._photographer.tagline}</p>
